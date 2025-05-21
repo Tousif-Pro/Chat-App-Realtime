8719daab04362b7c4311ffd5f054a860bceafd86
Tousif-Pro <tousifdev07@gmail.com>
2025-05-22 02:57:32 +0530

    updated controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -68,3 +68,7 @@ export const sendMessage = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const getUserForSidebar = () => {
+  // function implementation
+};
