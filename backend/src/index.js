c2eccaed932f926ee0455c7a288d460c19175034
Tousif-Pro <tousifdev07@gmail.com>
2025-05-22 02:00:30 +0530

    Render.com updated code

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,10 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 console.log("Starting the server...");
 
